a5c56635c491a26be52af76d497d8b93d735a445
Julian Blair <j.lee.blair@gmail.com>
2022-05-10 15:07:35 +0200

    Initial Layout

---
+++ src/components/InputForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p class="">Enter your JSON string to see File Tree</p>
+    <div class="split input-wrapper">
+      <input type="text" class="input-field" />
+      <input type="button" class="button text-center" value="button" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputForm",
+};
+</script>
+
+<style></style>
