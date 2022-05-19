b20fbdd2a3b3224b9e20151c3b8ef90a921a1df4
Julian Blair <j.lee.blair@gmail.com>
2022-05-19 18:14:08 +0200

    two unique folders with one file each, plus refactor

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,13 +1,10 @@
 import App from "../../src/App";
 import { mount } from "@vue/test-utils";
-import { setInputAndClickOk, selectors } from "./helpers";
+import { selectors, simulateClickWithValue, UserInput } from "./helpers";
 
 test("when string array valid and click ok, should switch to tree view", async () => {
   const wrapper = mount(App);
-  const stringArray = `["dc/character_list.txt"]`;
-  const inputField = wrapper.find(selectors.InputFormInputField);
-
-  await setInputAndClickOk(wrapper, inputField, stringArray);
+  await simulateClickWithValue(wrapper, UserInput.OneFolderOneFile);
 
   const treeComp = wrapper.find(selectors.TreeComponent);
   const inputForm = wrapper.find(selectors.InputForm);
@@ -15,12 +12,9 @@ test("when string array valid and click ok, should switch to tree view", async (
   expect(inputForm.exists()).toBeFalsy();
 });
 
-test("when input array has one folder and one file, render tree structure with two folders and one file", async () => {
+test("when input array has one folder and one file, render tree structure with one subfolder and one file", async () => {
   const wrapper = mount(App);
-  const stringArray = `["dc/character_list.txt"]`;
-  const inputField = await wrapper.find(selectors.InputFormInputField);
-
-  await setInputAndClickOk(wrapper, inputField, stringArray);
+  await simulateClickWithValue(wrapper, UserInput.OneFolderOneFile);
 
   const treeFolder = await wrapper.find(selectors.FolderComponentFolder);
   const treeFolderTitle = await wrapper.findAll(
@@ -33,12 +27,10 @@ test("when input array has one folder and one file, render tree structure with t
   expect(fileName.html()).toContain("character_list.txt");
 });
 
-test("when input has duplicate folder and two files, render tree structure with one folder and two file", async () => {
+test("when input has duplicate folder and two files, render tree structure with one subfolder and two file", async () => {
   const wrapper = mount(App);
-  const stringArray = `["dc/character_list.txt", "dc/the-doctor.png"]`;
-  const inputField = await wrapper.find(selectors.InputFormInputField);
+  await simulateClickWithValue(wrapper, UserInput.OneFoldersTwoFiles);
 
-  await setInputAndClickOk(wrapper, inputField, stringArray);
   const fileItems = await wrapper.findAll(selectors.FileComponentFileName);
   const treeFolders = await wrapper.findAll(selectors.FolderComponentFolder);
   const treeFolderTitle = await wrapper.findAll(
@@ -49,4 +41,20 @@ test("when input has duplicate folder and two files, render tree structure with 
   expect(treeFolders.length).toBe(2);
   expect(fileItems.at(0).html()).toContain("character_list.txt");
   expect(treeFolderTitle.at(1).html()).toContain("dc");
+});
+
+test("when input has two unique folders with one file each, render tree structure with two subfolders and each with one file", async () => {
+  const wrapper = mount(App);
+  await simulateClickWithValue(wrapper, UserInput.TwoFoldersOneFileEach);
+
+  const fileItems = wrapper.findAll(selectors.FileComponentFileName);
+  const treeFolders = wrapper.findAll(selectors.FolderComponentFolder);
+  const treeFolderTitle = wrapper.findAll(selectors.FolderComponentFolderTitle);
+
+  expect(fileItems.length).toBe(2);
+  expect(treeFolders.length).toBe(3);
+  expect(fileItems.at(0).html()).toContain("character_list.txt");
+  expect(fileItems.at(1).html()).toContain("the-doctor.png");
+  expect(treeFolderTitle.at(1).html()).toContain("dc");
+  expect(treeFolderTitle.at(2).html()).toContain("baz");
 });
--- tests/unit/helpers.js
+++ tests/unit/helpers.js
@@ -4,6 +4,12 @@ export async function setInputAndClickOk(wrapper, element, value) {
   button.trigger("click");
 }
 
+export async function simulateClickWithValue(wrapper, inputString) {
+  const inputField = await wrapper.find(selectors.InputFormInputField);
+
+  await setInputAndClickOk(wrapper, inputField, inputString);
+}
+
 export const selectors = {
   InputForm: '[t-id="InputForm"]',
   InputFormInputField: '[t-id="InputForm-input-field"]',
@@ -13,4 +19,10 @@ export const selectors = {
   FolderComponentFolder: '[t-id="Folder-folder"]',
   FolderComponentFolderTitle: '[t-id="Folder-folder-title"]',
   FileComponentFileName: '[t-id="File-file-name"]',
+};
+
+export const UserInput = {
+  TwoFoldersOneFileEach: `["dc/character_list.txt", "baz/the-doctor.png"]`,
+  OneFoldersTwoFiles: `["dc/character_list.txt", "dc/the-doctor.png"]`,
+  OneFolderOneFile: `["dc/character_list.txt"]`,
 };
